54f5dd08e3649759f0b32c2c00d9dffefce98899
aeSTet <76133815+aeSTet@users.noreply.github.com>
2021-10-08 17:23:49 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,15 +1,44 @@
+import threading
 import socket
-from threading import Thread
+from tqdm import tqdm
 
-N = 2**16 - 1
+adr = 'localhost'
+free_port = []
+start = 0
+finish = 65535
+NumbersOfThreads=3000
+one_percent= int((finish-start)/100)
+all_port = [i for i in range(start, finish)]
+procent = 0
+pbar = tqdm(total=100)
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+
+def port_connect(adr, port):
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    if conn.connect_ex((adr, port)):
+        return port
+    else:
+        return False
+
+
+def port_thread():
+    while all_port:
+        global procent
+        global loading
+        port = all_port.pop()
+        procent += 1
+        if procent % one_percent == 0:
+            pbar.update(1)
+        if port_connect(adr, port):
+            free_port.append(port)
+    pbar.close()
+
+threads = [threading.Thread(target=port_thread) for i in range(NumbersOfThreads)]
+
+[i.start() for i in threads]
+[i.join() for i in threads]
+print("\n")
+lenght=len(free_port)
+print(f"Список открытых портов (всего {lenght}):")
+print(sorted(free_port))
+print(len(free_port))
