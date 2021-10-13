7f3941be7e0dc7b0a727289fe530879209d727ac
HanyBin <51113403+HanyBin@users.noreply.github.com>
2021-10-13 17:55:50 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,27 +1,35 @@
 import threading
 import socket
-
+from tqdm import tqdm
+import random
 
 def portscan(port):
     global r
-    d = port * 1024
-    f = d - 1024
-    for i in range(f, d):
+    ports = []
+    d = 101
+    f = 1
+    ip = input()
+    for i in tqdm(range(f, d),desc="Port"):
         sock = socket.socket()
         sock.settimeout(0.5)
-        if i == r - 1:
-            print ("Сканирование завершено!")
         try:
-            connection = sock.connect(('127.0.0.1', i))
-            print('Порт :', i, "открыт.")
+            connection = sock.connect((ip, i))
+            ports.append(i)
             connection.close()
         except:
-            # print(i)
+            #print(i)
             pass
+    if len(ports) > 0:
+        print(f'Список свободных портов {ports}')
+        a = random.choice(ports)
+        ports.remove(a)
+        print(f'Порт {a} открыт')
+    else:
+        print('Нет свободных портов.')
 
 
-s = 4  # Введите число s от 1 до 64 (это число умножается на 1024) для проверки портов. 1 = 1024, 64 = 65536
-r = s * 1024
+s = 1  # Введите число s от 1 до 64 (это число умножается на 1024) для проверки портов. 1 = 1024, 64 = 65536
+r = s * 100
 print ("Запущено сканирование " + str(r) + " портов")
 for element in range(s + 1):
     t = threading.Thread(target=portscan, kwargs={'port': element})
