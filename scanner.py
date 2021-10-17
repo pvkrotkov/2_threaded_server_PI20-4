70dae221246627a68c38795f0d07b7e0940adf30
Юля Нагубнева <gubiky@mail.ru>
2021-10-18 01:56:24 +0300

    Добавляет код сканера

--- scanner.py
+++ scanner.py
@@ -1,15 +1,34 @@
 import socket
-from threading import Thread
+import concurrent.futures
+import tqdm
 
-N = 2**16 - 1
 
-for port in range(1,100):
+def scan_port(host, port, progress_bar, ports):
     sock = socket.socket()
+    sock.settimeout(0.5)
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
+        sock.connect((host, port))
+        ports.append(port)
     except:
-        continue
-    finally:
-        sock.close()
+        pass 
+    progress_bar.update()
+
+
+host = input('Введите хост: ')
+start_port = int(input('Введите минимальный порт: '))
+end_port = int(input('Введите максимальный порт: '))
+thread = int(input('Сколько потоков использовать для сканирования одновременно: '))
+
+ports = []
+progress_bar = tqdm.tqdm(total=end_port-start_port+1)
+
+with concurrent.futures.ThreadPoolExecutor(thread) as executor:
+    futures = []
+    for port in range(start_port, end_port+1):
+        future = executor.submit(scan_port, host, port, progress_bar, ports)
+        futures.append(future)
+    for future in futures:
+        future.result()
+progress_bar.close()
+for port in ports:
+    print(f'Порт {port} открыт!')
