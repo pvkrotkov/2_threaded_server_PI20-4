4febb02ebfdcc5aa8f1a69ce499bfbb09b5ea970
Zevety <90498783+Zevety@users.noreply.github.com>
2021-11-11 20:54:46 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,42 @@
 import socket
-from time import sleep
+import threading
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def main():
+    name = ''
+    while not name:
+        name = input('Ваше имя: ')
 
-data = sock.recv(1024)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 9091))
+    t1 = threading.Thread(target=sending, args=[sock, name])
+    t2 = threading.Thread(target=receiving)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    sock.close()
 
-sock.close()
 
-print(data.decode())
+def sending(sock, name):
+    sock.send(f'Клиент {name} присоединился'.encode())
+    while True:
+        mes = input(': ')
+        sock.send(f'{name}: {mes}'.encode())
+        if mes == 'exit':
+            sock.close()
+            break
+
+
+def receiving():
+    while True:
+        try:
+            data = sock.recv(1024)
+            print(data.decode())
+        except:
+             print('Выход.')
+             break
+
+
+if __name__ == '__main__':
+    main()
